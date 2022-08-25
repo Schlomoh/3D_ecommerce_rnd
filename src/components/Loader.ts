6640d524443040e45bc26464297f9455773dd201
Schlomoh <post@moritzbecker.de>
2022-08-25 19:32:00 +0200

    add draco loading

--- src/components/Loader.ts
+++ src/components/Loader.ts
@@ -1,14 +1,20 @@
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
 import ModelScene from "./ModelScene";
 import { ObjectModifications } from "./utils";
 
 class Loader {
   private scene: ModelScene;
   private gltfLoader = new GLTFLoader();
+  private dracoLoader = new DRACOLoader();
+  private dracoDecoderPath =
+    // "https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/";
+    "/decoder/draco/"
 
   constructor(scene: ModelScene) {
     this.scene = scene;
+    this.dracoLoader.setDecoderPath(this.dracoDecoderPath);
+    this.gltfLoader.setDRACOLoader(this.dracoLoader);
   }
 
   private onLoad(self: this, gltf: GLTF) {
